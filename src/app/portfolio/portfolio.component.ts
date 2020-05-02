d5acb3e7883a464877d1b2529a2ea5ecf40b2908
Nouran <nourans.96@yahoo.com>
2020-05-02 10:39:14 +0200

    Filter projects according to category

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,11 +10,29 @@ import Projects from './projects.interface';
 export class PortfolioComponent implements OnInit {
 
   projects = [];
+  filteredProjects = [];
+  activeBtn:string = 'all';
 
   constructor(private portfolioService: PortfolioService) { }
 
   ngOnInit(): void {
     this.projects = this.portfolioService.getProjects();
+    this.filteredProjects = this.projects;
+  }
+
+  showAll() {
+    this.activeBtn = 'all';
+    this.filteredProjects = this.projects;
+  }
+
+  showFrontOnly() {
+    this.activeBtn = 'front-end';
+    this.filteredProjects = this.projects.filter((project) => project.category == 'front-end')
+  }
+
+  showBackOnly() {
+    this.activeBtn = 'back-end';
+    this.filteredProjects = this.projects.filter((project) => project.category == 'back-end')
   }
 
 }
